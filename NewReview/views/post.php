c66c34120c7cc7793371ccb581e3854127039bd1
Rangerine <rumplesstilkins@gmail.com>
2016-06-11 03:23:26 -0500

    Update post.php
    Added form input fields to the "New Review" post form.

--- NewReview/views/post.php
+++ NewReview/views/post.php
@@ -3,7 +3,22 @@ $Session = Gdn::Session();
 $CancelUrl = '/discussions';
 if (C('Vanilla.Categories.Use') && is_object($this->Category))
    $CancelUrl = '/categories/'.urlencode($this->Category->UrlCode);
+   
+    //options for dropdowns
+    // In HTML you have input type text and textareas. Vanilla only has TextBoxes.
+    // In order to make them multiline, you need an options array like that.
+    $textBoxOptions = $this->Data('TextBoxOptions');
+    // Some Example Data for a RadioList.
+    $radioListData = $this->Data('RadioListData');
+    // You can define which radio button should be preselected.
+    $radioListOptions = $this->Data('RadioListOptions');
 
+    // For a dropdown, you can use the same data as for the RadioList.
+    $dropDownData = $radioListData;
+    // But for setting a standard value, you have to user "value".
+    $dropDownOptions = $this->Data('DropDownOptions');   
+   
+   //end options for dropdowns
 ?>
 <div id="DiscussionForm" class="FormTitleWrapper DiscussionForm">
    <?php
@@ -24,10 +39,34 @@ if (C('Vanilla.Categories.Use') && is_object($this->Category))
 			echo '</div>';
       }
 
-      echo '<div class="P">';
+          echo '<div class="P">';
 			echo $this->Form->Label('Review', 'Name');
 			echo Wrap($this->Form->TextBox('Name', array('maxlength' => 100, 'class' => 'InputBox BigInput')), 'div', array('class' => 'TextBoxWrapper'));
 		echo '</div>';
+		//Form Input Fields Start
+				//Product Name
+	  echo '<div class="P">';
+			echo $this->Form->Label('Product Name', 'TestField0');
+			echo Wrap($this->Form->TextBox('TestField0', array('maxlength' => 100, 'class' => 'InputBox BigInput')), 'div', array('class' => 'TextBoxWrapper'));
+		echo '</div>';
+		//Date Purchased
+	  echo '<div class="P">';
+			echo $this->Form->Label('Date Purchased', 'TestField1');
+			echo Wrap($this->Form->TextBox('TestField1', array('maxlength' => 100, 'class' => 'InputBox BigInput')), 'div', array('class' => 'TextBoxWrapper'));
+		echo '</div>';		
+		//Location Purchased, amazon, newegg, etc.	
+	  echo '<div class="P">';
+			echo $this->Form->Label('Location Purchased', 'TestField1');
+			echo Wrap($this->Form->TextBox('TestField1', array('maxlength' => 100, 'class' => 'InputBox BigInput')), 'div', array('class' => 'TextBoxWrapper'));
+		echo '</div>';	
+		//Recommended?
+	  echo '<div class="P">';
+			echo $this->Form->Label('Recommended', 'RadioListExample');
+			echo Wrap($this->Form->DropDown('RadioListExample', $dropDownData, $dropDownOptions));
+		echo '</div>';		
+		//Form Input Fields End
+		
+		
 		$this->FireEvent('BeforeBodyInput');
 		echo '<div class="P">';
          echo $this->Form->BodyBox('Body', array('Table' => 'Discussion', 'FileUpload' => true));
